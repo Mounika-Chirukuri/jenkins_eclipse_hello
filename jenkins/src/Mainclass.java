e8c63423a29901670bb75395b062496983a6723f
miracle <miracle@MSS>
2015-09-16 09:40:23 -0400

    2on got modified

--- jenkins/src/Mainclass.java
+++ jenkins/src/Mainclass.java
@@ -6,7 +6,7 @@ public class Mainclass {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hi other proj");
+System.out.println("hi other proj1");
 	}
 
 }
